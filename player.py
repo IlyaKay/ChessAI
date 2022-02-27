401b4f22b721adaca22da068e4b20e30225a6595
IlyaKay <kiselev.ilya@gmail.com>
2022-02-27 22:26:07 +0000

    Revert "Test commit of comment."
    This reverts commit 2fd48c57

--- player.py
+++ player.py
@@ -72,7 +72,7 @@ class AiPlayer(Player):
    @pyqtSlot(str)
    def TakeTurn(self, fen):
       """
-         BRIEF   Evaluation FEN position with chess engine
+         BRIEF   Open a process to get the next move from the AI
                  Emit DecidedMove(uci) and return uci
       """
       self.last_fen = fen
